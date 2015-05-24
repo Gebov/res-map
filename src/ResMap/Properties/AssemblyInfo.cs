bd55e316f1932ad1029cdd00af2bbb5dbc386a73
gebov <martin.gebov@gmail.com>
2015-05-24 21:45:07 +0300

    Renamed assembly info

--- src/ResMap/Properties/AssemblyInfo.cs
+++ src/ResMap/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gebov.Res.MsBuild")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ResMap")]
+[assembly: AssemblyDescription("Provides mappings between embedded resources and local file system resources.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gebov.Res.MsBuild")]
+[assembly: AssemblyProduct("ResMap")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
